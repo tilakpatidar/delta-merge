56b98eba1c7c97ca5e094149fa21de458d319014
tilakpatidar <tilakpatidar@gmail.com>
2018-08-14 15:30:28 +0530

    Suppress spark INFO logs

--- scripts/lib_python/main/__main__.py
+++ scripts/lib_python/main/__main__.py
@@ -5,6 +5,8 @@ from pyspark import SparkContext, HiveContext
 
 job_path = sys.argv[1]
 sc = SparkContext()
+# Suppress spark console logs to error
+sc.setLogLevel("ERROR")
 hive_context = HiveContext(sc)
 job_config = json.loads(open(job_path, "r").read())
 build_latest_snapshot_and_delta.main((sc, hive_context), job_config)
